ef45e9b2dd72eef4e8721b9e93355f3c8d82ac81
MKASIF <mdasifkhan089@gmail.com>
2022-05-07 00:16:09 +0600

    Create some necessary pages

---
+++ src/components/pages/StokItem/StokItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StokItem = () => {
+    return (
+        <div>
+            <h1>This is stok item page</h1>
+        </div>
+    );
+};
+
+export default StokItem;
---
+++ src/components/share/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>This is nav bar</h1>
+        </div>
+    );
+};
+
+export default Navbar;
